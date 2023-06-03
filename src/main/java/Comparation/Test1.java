0075b9fcbdf3deb83522d410893afc55fcf0475f
VanPirg <ivan.files.09@gmail.com>
2023-06-03 12:30:35 +0300

    03/06/2023

---
+++ src/main/java/Comparation/Test1.java
@@ -0,0 +1,19 @@
+package Comparation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Zaur");
+        list.add("Ivan");
+        list.add("Mariya");
+        System.out.println("Pered Sortirovkoy");
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println("Posle sortirovki");
+        System.out.println(list);
+    }
+}
---
+++ src/main/java/Comparation/Test2.java
@@ -0,0 +1,96 @@
+package Comparation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args) {
+        List<Employee> list = new ArrayList<>();
+        Employee emp1 = new Employee(100,"Zaur","Tregulov",12000);
+        Employee emp2 = new Employee(15,"Ivan","Petrov",6542);
+        Employee emp3 = new Employee(100,"Ivan","Sidorov",8542);
+        list.add(emp1);
+        list.add(emp2);
+        list.add(emp3);
+        System.out.println("Before sorting \n" + list);
+        Collections.sort(list );
+        System.out.println("after sorting \n" + list);
+    }
+}
+
+
+class Employee
+        implements Comparable<Employee>
+{
+    int id;
+    String name;
+    String surname;
+    int salary;
+
+    public Employee(int id, String name, String surname, int salary) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee anotherEmp) {
+        if (this.id == anotherEmp.id){
+            return 0;
+        }
+        else if (this.id < anotherEmp.id){
+            return -1;
+        }
+        else {
+            return 1;
+        }
+////        return this.id - anotherEmp.id;
+////        int res = this.name.compareTo(anotherEmp.name);
+////        if (res == 0){
+////            res = this.surname.compareTo(anotherEmp.surname);
+////        }
+////        return res;
+    }
+}
+//class idComparator implements Comparator<Employee>{
+//
+//    @Override
+//    public int compare(Employee emp1, Employee emp2) {
+//        if (emp1.id == emp2.id){
+//            return 0;
+//        }
+//        else if (emp1.id < emp2.id){
+//            return -1;
+//        }
+//        else {
+//            return 1;
+//        }
+//    }
+//}
+class  nameComparator implements Comparator<Employee>{
+
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+       return emp1.name.compareTo(emp2.name);
+    }
+}
+class  SalaryComparator implements Comparator<Employee>{
+
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        return emp1.salary - emp2.salary;
+    }
+}
